787ac3da1264ceef42b41535e155296905922b3b
Charles Dufour <charles.dufour@epfl.ch>
2019-09-06 15:07:27 +0200

    initialization

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Comparison of standard combinatorial optimization with reinforcement learning techniques on rescheduling problems',
+    author='Charles Dufour, Edouard Ghaleb',
+    license='',
+)
