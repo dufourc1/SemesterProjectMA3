d280f14debbe1146d2fda8bd7706acaac333db36
Charles Dufour <charles.dufour@epfl.ch>
2019-09-23 19:35:55 +0200

    added first build of the graph, needs to be checked visually

--- src/benchmark/create_env.py
+++ src/benchmark/create_env.py
@@ -3,6 +3,8 @@ import networkx as nx
 import random
 import time
 
+from utils import *
+
 from flatland.envs.observations import *
 from flatland.envs.rail_env import RailEnv
 from flatland.envs.rail_generators import complex_rail_generator
@@ -10,6 +12,8 @@ from flatland.envs.schedule_generators import complex_schedule_generator
 from flatland.utils.rendertools import RenderTool
 
 
+import matplotlib.pyplot as plt
+import json
 
 class EnvWrapperGraph(RailEnv):
 	'''
@@ -27,17 +31,65 @@ class EnvWrapperGraph(RailEnv):
 						schedule_generator,number_of_agents,
 						obs_builder_object)
 
-		self.graph = self.create_graph_from_env(self.obs_builder)
+		self.graph_high_level, self.graph_low_level = self.create_graph_from_env(self.obs_builder)
 
 
 
 	def create_graph_from_env(self,observation_builder):
-		graph = nx.Graph()
-		graph.add_node((1,2))
-		return  graph
+
+		#initialize the graphs
+		graph_low_level = nx.DiGraph()
+		graph_high_level = nx.DiGraph()
+
+		#get the matrix representing the environment
+		matrix_rail = np.array(self.rail.grid.tolist())
+
+		self.__add_nodes(graph_high_level,graph_low_level,matrix_rail)
+
+		self.__add_edges(graph_high_level,graph_low_level,matrix_rail)
+		
+		return  graph_high_level, graph_low_level
 
 
-	def show_graph(self,mode = 'spectral', options = None):
+	def __add_edges(self,graph_high_level,graph_low_level,matrix_rail):
+
+		for index,value in np.ndenumerate(matrix_rail):
+			#self.__create_edges_one_cell(index,value,graph_high_level,'high')
+			graph_low_level = self.__create_edges_one_cell(index,value,graph_low_level,'low')
+
+
+
+	def __create_edges_one_cell(self,index,value,graph,level = 'high'):
+		binary_rep = cell_to_byte(value)
+		name_node = tuple_to_str(index)
+		if level == 'low':
+			results = identify_crossing(binary_rep)
+			for key,goals in results.items():
+				e1 = name_node + CONVENTION_FROM[key]
+				for goal in goals:
+					e2 = name_node +CONVENTION_TOWARDS[goal]
+					graph.add_edge(e1,e2)
+			return graph
+		elif level == 'high':
+			raise NotImplementedError
+		else:
+			raise ValueError(f"only implementation for low and high level graphs, not {level}")
+
+	
+	def __add_nodes(self, graph_high_level,graph_low_level, matrix_rail):
+
+		#add the rails to the graph as nodes (rails are positive in the matrix)
+		list_nodes = [tuple_to_str(index) for index, x in np.ndenumerate(matrix_rail) if x >0 ]
+		graph_high_level.add_nodes_from(list_nodes)
+
+
+		for elt in list_nodes:
+			graph_low_level.add_node(elt + "a")
+			graph_low_level.add_node(elt + "b")
+
+
+
+	def show_graph(self,mode = 'spectral', options = None, node_names = False):
 		'''
 		visualization of the graph
 		'''
@@ -53,7 +105,10 @@ class EnvWrapperGraph(RailEnv):
 					'width': 3,
 				}
 		if mode == 'spectral':
-			nx.draw_spectral(env.graph, **options)
+			nx.draw_spectral(env.graph_low_level,with_labels=node_names, **options)
+			plt.show()
+		elif mode == 'classic':
+			nx.draw(env.graph_low_level,with_labels=node_names, **options)
 			plt.show()
 		else:
 			raise ValueError(f'this mode of projection ({mode}) is not implemented yet')
@@ -64,7 +119,7 @@ if __name__ == '__main__':
 	random.seed(1)
 	np.random.seed(1)
 
-	number_agents = 2
+	number_agents = 4
 
 
 	env = EnvWrapperGraph(width=7,
@@ -75,12 +130,16 @@ if __name__ == '__main__':
               obs_builder_object=GlobalObsForRailEnv())
 
 	env.reset()
-	matrix = np.array(env.rail.grid.tolist())
 
-	print(matrix.shape)
-	print(matrix)
 
 	
 	env_renderer = RenderTool(env)
 	env_renderer.render_env(show=True, show_predictions=False, show_observations=True)
-	time.sleep(20)
+	time.sleep(0.3)
+
+	env.show_graph(mode = 'classic', node_names=False)
+	G = env.graph_low_level
+	print(G.edges)
+	pos = dict( (n, str_to_tuple(n[:-1])) for n in G.nodes() )
+	nx.draw(G,pos = pos)
+	plt.show()
---
+++ src/benchmark/utils.py
@@ -0,0 +1,116 @@
+import numpy as np
+from flatland.core.grid.grid4 import Grid4Transitions
+
+
+LISTE_TRANSITIONS =   [int('0000000000000000', 2),  # empty cell - Case 0
+                       int('1000000000100000', 2),  # Case 1 - straight
+                       int('1001001000100000', 2),  # Case 2 - simple switch
+                       int('1000010000100001', 2),  # Case 3 - diamond drossing
+                       int('1001011000100001', 2),  # Case 4 - single slip
+                       int('1100110000110011', 2),  # Case 5 - double slip
+                       int('0101001000000010', 2),  # Case 6 - symmetrical
+                       int('0010000000000000', 2),  # Case 7 - dead end
+                       int('0100000000000010', 2),  # Case 1b (8)  - simple turn right
+                       int('0001001000000000', 2),  # Case 1c (9)  - simple turn left
+                       int('1100000000100010', 2)]  # Case 2b (10) - simple switch mirrored
+
+	
+
+trad_direction = {
+	0: 'N',
+	1: 'E',
+	2: 'S',
+	3: 'W'
+}
+
+#going towards this direction we must link to the node in the key
+CONVENTION_TOWARDS = {
+	'W':'a',
+	'S':'a',
+	'N':'b',
+	'E':'b'
+}
+
+#going from this direction we must link to the node in the key
+CONVENTION_FROM = {
+	'W':'b',
+	'S':'b',
+	'N':'a',
+	'E':'a'
+}
+
+def tuple_to_str(x, filled = 4):
+	'''
+	(x,y) --> '000x000y'
+	'''
+	return str(x[0]).zfill(filled)+str(x[1]).zfill(filled)
+
+
+def str_to_tuple(x_string):
+	'''
+	'000x000y' --> (x,y)
+	'''
+	return (int(x_string[0:4]),int(x_string[4:]))
+	
+
+
+def print_cell_transition(cell_transition):
+	'''
+	pretty printing of cell transition in 16 byte
+	'''
+	print("  NESW")
+	print("N", format(cell_transition >> (3 * 4) & 0xF, '04b'))
+	print("E", format(cell_transition >> (2 * 4) & 0xF, '04b'))
+	print("S", format(cell_transition >> (1 * 4) & 0xF, '04b'))
+	print("W", format(cell_transition >> (0 * 4) & 0xF, '04b'))
+
+
+def cell_to_byte(cell_info):
+	return bin(cell_info)[2:]
+
+
+def get_direction(binary_4):
+	results = []
+	for i, elt in enumerate(binary_4):
+		if elt == '1':
+			results.append(trad_direction[i])
+	return results
+
+
+
+def identify_crossing(matrix_rail_element):
+
+
+	# Translations_manip = Grid4Transitions(LISTE_TRANSITIONS)
+
+	byte_representation = bin(int(matrix_rail_element))[2:]
+	# count_of_ones = byte_representation.count('1')
+	# if count_of_ones == 0:
+	# 	return 'empty cell'
+	# elif count_of_ones == 1:
+	# 	return 'dead end'
+	# elif count_of_ones == 2:
+	# 	return 'simple'
+	# elif count_of_ones == 6:
+	# 	return 'double slip'
+	# elif count_of_ones == 8:
+	# 	return 'symetrical'
+	# else:
+	# 	if byte_representation == bin(LISTE_TRANSITIONS[3])[2:0]:
+	# 		return 'diamond crossing'
+	# 	else:
+	# 		return 'fuck'
+
+
+	#get the possible actions based on from where the agent is arriving
+	byte_north = byte_representation[0:4]
+	byte_east = byte_representation[4:8]
+	byte_south = byte_representation[8:12]
+	byte_west = byte_representation[12:16]
+
+	return {
+		'N':get_direction(byte_north),
+		'E': get_direction(byte_east),
+		'S': get_direction(byte_south),
+		'W':get_direction(byte_west)
+	}
